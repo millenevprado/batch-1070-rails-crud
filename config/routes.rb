798cb6b5237eeab5daf266c020a74e14e9ac5e02
Millene Prado <millene_vprado@hotmail.com>
2022-11-15 11:40:57 -0300

    finished crud and refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # verb 'path', to: 'controller#action'
+  # READ all restaurants
+  get 'restaurants', to: 'restaurants#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # CREATE a restaurant
+  get 'restaurants/new', to: 'restaurants#new'
+  post 'restaurants', to: 'restaurants#create'
+
+  # READ one restaurant
+  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  # UPDATE a restaurant
+  get 'restaurants/:id/edit', to: 'restaurants#edit'
+  patch 'restaurants/:id', to: 'restaurants#update'
+
+  # DELETE a restaurant
+  delete 'restaurants/:id', to: 'restaurants#destroy'
+
+  # resources :restaurants, except: [:destroy]
 end
